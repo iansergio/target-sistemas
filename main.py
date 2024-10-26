fe20b839da6b72b5164c63947e6de1e7c0e9bc28
ian <iansergiohelf@gmail.com>
2024-10-26 17:53:46 -0300

    feat: Adicionando questão 04

--- main.py
+++ main.py
@@ -28,3 +28,11 @@ print(verificaString("O", "Sou uma string a ser verificada"))
 print("3) Trecho de código")
 # Resposta
 print("O valor será 77") 
+
+# 4) Descubra a lógica e complete o próximo elemento:
+# a) 1, 3, 5, 7, [9]
+# b) 2, 4, 8, 16, 32, 64, [128]
+# c) 0, 1, 4, 9, 16, 25, 36, [49]
+# d) 4, 16, 36, 64, [100]
+# e) 1, 1, 2, 3, 5, 8, [13]
+# f) 2, 10, 12, 16, 17, 18, 19, [200]
