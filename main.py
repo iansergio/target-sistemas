da78ac3552edc3335f3e26ba4baa3ebce97b2fb0
ian <iansergiohelf@gmail.com>
2024-10-26 18:31:12 -0300

    feat: Adicionando questão 05

--- main.py
+++ main.py
@@ -25,14 +25,16 @@ print("2 Verificar existência da letra 'a')")
 print(verificaString("O", "Sou uma string a ser verificada"))
 
 # 3) Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
-print("3) Trecho de código")
-# Resposta
-print("O valor será 77") 
+# Resposta: 77
 
 # 4) Descubra a lógica e complete o próximo elemento:
+# Respostas entre colchetes "[ ]"
 # a) 1, 3, 5, 7, [9]
 # b) 2, 4, 8, 16, 32, 64, [128]
 # c) 0, 1, 4, 9, 16, 25, 36, [49]
 # d) 4, 16, 36, 64, [100]
 # e) 1, 1, 2, 3, 5, 8, [13]
 # f) 2, 10, 12, 16, 17, 18, 19, [200]
+
+# 5) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes. Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada. Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?
+# Resposta: Com o calor gerado pela lâmpada. Na primeira ida eu deixaria ligado o primeiro interruptor por alguns minutos e depois desligaria o primeiro e ligaria o segundo interruptor, então iria até a primeira sala, se a lâmpada estivesse ligada: a sala 1 tem conexão com o interruptor 2, se estivesse desligada porém ao tocar a lâmpada ela estivesse morna: a sala 1 tem conexão com o interruptor 1, e se ao tocar a lâmpada ela estivesse fria: a sala 1 tem conexão com o interruptor 3. E na segunda ida seria da mesma forma.
