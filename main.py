c28b80d78d26e2165171da639f5cff351ce32520
ian <iansergiohelf@gmail.com>
2024-10-26 17:04:28 -0300

    feat: Adicionando questão 01

---
+++ main.py
@@ -0,0 +1,18 @@
+# 1) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
+def verificaEmFibonnaci(n):
+    primeiroValor = 0
+    segundoValor = 1
+    # print(primeiroValor)
+    while segundoValor < n:
+        # print(segundoValor)
+        # Atribuindo a um valor auxiliar a soma dos dois valores
+        aux = primeiroValor + segundoValor
+        # Atribuindo o valor do segundo valor ao primeiro e o do valor auxiliar ao segundo 
+        primeiroValor = segundoValor
+        segundoValor = aux
+        if segundoValor == n:
+            print(f"O número {n} faz parte da sequência de Fibonacci.")
+        elif segundoValor > n and segundoValor != n:
+            print(f"O número {n} não pertence à sequência de Fibonacci.")    
+print("1) Fibonacci")
+verificaEmFibonnaci(7778742049)
